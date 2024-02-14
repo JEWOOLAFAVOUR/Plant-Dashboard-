043f13dcd9142a8f0dc97b459b1715366ef372bb
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-02-14 15:08:37 -0800

    working on the styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './App.css'
+import SideBar from './components/SideBarSection/SideBar'
+import Body from './components/BodySection/Body/Body'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="container">
+      <SideBar />
+      <Body />
+    </div>
   )
 }
 
