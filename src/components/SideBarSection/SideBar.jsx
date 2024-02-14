00c2cfa111a25be08d3c50af06ca6ef23aa90102
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-02-14 15:48:14 -0800

    new pushsh

--- src/components/SideBarSection/SideBar.jsx
+++ src/components/SideBarSection/SideBar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './sidebar.css'
 
+// IMPORTED IMAGES 
+import flower1 from '../../assets/images/flower1.jpg'
+
+
 const SideBar = () => {
     return (
-        <div>SideBar</div>
+        <div className='sideBar grid  '>
+            <div className="logDiv flex">
+                <img src={flower1} alt="Image Name" />
+                <h2>Planti.</h2>
+            </div>
+
+            <div className="menuDiv">
+                <h3 className="divTitle">QUICK MENU</h3>
+                <ul className="menuLists grid">
+
+                    <li className="listItem">
+                        <a href="" className="menuLink"></a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
     )
 }
 
